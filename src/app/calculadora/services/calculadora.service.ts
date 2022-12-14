2a79c95778c02c0b2b9abf15f71267b5ceb9e23b
Sávio Bertoldi <77998135+saviuu@users.noreply.github.com>
2022-12-14 18:52:35 -0300

    Criando método para cálculo + instalando bootstrap no projeto

--- src/app/calculadora/services/calculadora.service.ts
+++ src/app/calculadora/services/calculadora.service.ts
@@ -5,5 +5,41 @@ import { Injectable } from '@angular/core';
 })
 export class CalculadoraService {
 
+  /*Constantes para identificar as operações de cálculo*/
+  static readonly SOMA: string = '+';
+  static readonly SUBTRACAO: string = '-';
+  static readonly DIVISAO: string = '/';
+  static readonly MULTIPLICACAO: string = '*';
+
   constructor() { }
+
+  /**
+   * Método que calcula uma operação matemática dado
+   * dois números.
+   * Suporta as operações soma, subtração, divisão,
+   * e multiplicação.
+   **/
+
+  calcular(num1: number, num2: number, operacao: string): number {
+    var resultado: number; //armazena o resultado da operação
+
+    switch(operacao){
+      case CalculadoraService.SOMA:
+        resultado = num1 + num2;
+      break;
+      case CalculadoraService.SUBTRACAO:
+        resultado = num1 - num2;
+      break;
+      case CalculadoraService.DIVISAO:
+        resultado = num1 / num2;
+      break;
+      case CalculadoraService.MULTIPLICACAO:
+        resultado = num1 * num2;
+      break;
+      default:
+        resultado = 0;
+    }
+
+    return resultado;
+  }
 }
